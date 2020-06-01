003281dae697290fbcb8a424a33f5540a6141833
kklldog <kklldog@qq.com>
2020-06-01 17:36:38 +0800

    update

---
+++ BlazorWasmDataBind/UserInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmDataBind
+{
+    public class UserInfo
+    {
+        public string UserName { get; set; }
+
+        public string Sex { get; set; }
+
+        public DateTime BrithDay { get; set; }
+    }
+}
