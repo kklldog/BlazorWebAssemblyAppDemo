ef87ccc5538a279019b86cb950b6b71b46694c65
kklldog <kklldog@qq.com>
2020-06-05 13:31:08 +0800

    update

--- BlazorWasmDataBind/UserInfo.cs
+++ BlazorWasmDataBind/UserInfo.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BlazorWasmDataBind
 {
-    public class UserInfo
+    public class UserInfo: INotifyPropertyChanged
     {
-        public string UserName { get; set; }
+        private string _userName;
+        public string UserName {
+            get
+            {
+                return _userName;
+            }
+            set
+            {
+                _userName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(UserName)));
+            }
+         }
 
         public string Sex { get; set; }
 
         public DateTime BrithDay { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
